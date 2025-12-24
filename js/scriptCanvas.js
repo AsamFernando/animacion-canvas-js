d74569c859fb4be71fad7083a2b800cdedf52dc3
AsamFernando <ferasam88@gmail.com>
2025-12-24 01:22:29 -0300

    agregadas actividades y comentarios a todo.md

--- js/scriptCanvas.js
+++ js/scriptCanvas.js
@@ -67,46 +67,6 @@ switchAnimacionBtn.addEventListener('click', switchLoop)
 //CORRECCIONES
 //--
 
-//proximos cambios
-
-//refactorizar las funciones y eventos y usar objetos donde se necesite
-//evitar mucho uso de if else
-//hacer que la velocidad sea constante sin depender de refresh rate del monitor, con timestamp
-//incorporar aceleracion al rectangulo
-//incorporar gravedad y su aceleracion
-//refactorizar las funciones que verifican las colisiones
-//que el rectangulo pueda pegar un salto presionando la w o barra espaciadora
-//que al pegar un salto se pueda desplazar en el aire con w y s
-//incorporar desplazamiento en diagonal
-//poder colisionar con cualquier rectangulo del listado
-//incorporar colisiones con objetos distintos de rectangulos, por ej circulo o rombo
-//agregar una rama con todos los comentarios del codigo
-//hacer una clase rectagulo o forma que reciba la cantidad de rects o huecos y cree
-//calculando los valores de ancho alto y tamaño en base a las cantidades y los ubique de manera
-//equivalente en el canvas
-//por el momento los rectangulos son multiplos de 5 para acertar colisiones por posicion de x e y
-//ver flujo de estudio con comentarios ramas e historial de ramas y commits
-//ver si conviene separa las funciones para dibujar en canvas y las q ejecutan o cancelan la animacion
-//juntar comentarios generales del codigo o comportamiento de js y dom/navegador/node etc y separar de
-//los que son explicando el codigo
-
-//COMPLETADOS
-//incorporar colisiones con otros rectangulo -> hecho
-//agregar inputs para dar una posicion inicial -> hecho
-//BUG: al colisionar las aristas de player con el rectangulo no permite avanzar hacia el rectangulo desde ninguno de los
-//4 lados -> solucionado
-//hacer una clase rectangulo q va a permitir agregar mas funcionalidad al player y escenario 
-//a la que le paso los minimos valores de representacion y tienen las
-//funciones para posicionarse y colisionar -> hecho
-//poner en archivo a parte el player los rects y la creacion de entidades -> hecho
-//crear archivo UI para los inputs de posicion, botones y cuadricula -> hecho
-//dividir en varios archivos tipo, colisiones.js, keys.js, inputs.js, player.js, y objects.js ... etc., hasta dejar solo draw() en este archivo. -> hecho
-//sacar funciones fuera de draw o crear nuevas -> hecho
-
-//COMENTARIOS GRAL
-//en js modules no se usan los index igual que con vite, solo especificando la carpeta q contiene el index, porque el navegador no lo encuentra,
-//hay q especificar index.js para poder tomar las exportaciones q necesito del objeto q las junta en index
-
 //COMENTARIOS EN DRAW()
 // const draw = () => {
     //     FPS++
