42d4564d409079a55b2a077a474eec0065743022
AsamFernando <ferasam88@gmail.com>
2025-12-29 23:28:48 -0300

    cambios en constantes y parametros para ver comportamiento de colisiones con distintas velocidades

--- js/constantes.js
+++ js/constantes.js
@@ -1,4 +1,5 @@
 //archivo solo de exportacion de constantes agregado para no interferir con otros imports y exports
 //y no crear referencias circulares o uso de variables o constantes no inicializadas
+const dts = [16.67, 8.34, 10]
 
-export const fixed_dt = 16.67
+export const fixed_dt = dts[0]
